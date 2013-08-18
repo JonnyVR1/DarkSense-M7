15635a758f494c2cf34a66a5fd5d08f0cd1c6ad1
Jonathon Fitch <jon1.fitch@hotmail.com>
2013-08-18 03:10:37 +0100

    Added more GPU steps - including stock 400MHz

--- arch/arm/mach-msm/board-m7-gpu.c
+++ arch/arm/mach-msm/board-m7-gpu.c
@@ -203,9 +203,24 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
+			.gpu_freq = 400000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 380000000,
+			.bus_freq = 3,
+			.io_fraction = 33,
+		},
+		{
 			.gpu_freq = 320000000,
 			.bus_freq = 3,
 			.io_fraction = 33,
+		},
+		{
+			.gpu_freq = 250000000,
+			.bus_freq = 2,
+			.io_fraction = 100,
 		},
 		{
 			.gpu_freq = 200000000,
@@ -218,7 +233,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 2,
-	.num_levels = 4,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
